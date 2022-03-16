b019e81a712f12e9364385c4954c5fa3259b3b89
ZaidanNur <zaixyz332@gmail.com>
2022-03-17 03:02:10 +0700

    add project week3

---
+++ Week3/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3
+{
+    public class Kalkulator
+    {
+        double x = 5;
+        double y = 4;
+
+        //Mobil mobil = new Mobil("mini cooper", "bensin", "hitam");
+
+        public double Tambah()
+        {
+            //mobil.MatikanMesin();
+            return x + y;
+        }
+
+        public void setX(double angka)
+        {
+            this.x = angka;
+        }
+
+        public double getX()
+        {
+            return x;
+        }
+
+
+
+    }
+
+    public class Mobil
+    {
+        string model;
+        string bahan_bakar;
+        string warna;
+
+        public Mobil(string model, string bahan_bakar, string warna)
+        {
+            this.model = model;
+            this.bahan_bakar = bahan_bakar;
+            this.warna = warna;
+            Console.WriteLine("Ini dari contructor");
+        }
+
+
+
+        public void HidupkanMesin()
+        {
+            Console.WriteLine("Mesin hidup");
+        }
+
+        public void MatikanMesin()
+        {
+            Console.WriteLine("Mesin mati");
+        }
+    }
+}
---
+++ Week3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace Week3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mobil mini_cooper = new Mobil("mini cooper","bensin","hitam");
+
+            //mini_cooper.MatikanMesin();
+
+            //Kendaraan.Mobil lambo = new Kendaraan.Mobil();
+
+            //lambo.Posisi();
+            
+            Kalkulator kalkulator = new Kalkulator();
+
+            
+            Console.WriteLine(kalkulator.Tambah());
+
+            Console.WriteLine("===========");
+
+            kalkulator.setX(10);
+
+            Console.WriteLine(kalkulator.Tambah());
+
+            Console.WriteLine("===========");
+
+            Console.WriteLine(kalkulator.getX());
+
+
+        }
+    }
+
+    
+
+}
+
+namespace Kendaraan
+{
+    class Mobil
+    {
+        public void Posisi()
+        {
+            Console.WriteLine("Aku di namespace kendaraan");
+        }
+    }
+}
